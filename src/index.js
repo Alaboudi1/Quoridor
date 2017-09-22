4f38c538c48b28b10078fe66071cb854f5a42477
Abdulaziz <alobodi1@hotmail.com>
2017-09-22 16:46:18 -0400

    boilerplate for Quoridor game

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// Here should go all the javascript code, you could aslo import whatever you want for example:
+
+import {logic} from './logic';
+
+class index {
+    constructor() {
+        new logic().sayHello('Abdulaziz'); // this going to print Hello to Abdulaziz in the console
+    }
+}
+
+new index();
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+var pro = process.env.NODE_ENV === 'production';
+var entry = pro
+    ? ['./src/index.js']
+    : [
+        './src/index.js',
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080'
+    ];
+var plugins = pro
+    ? []
+    : [new webpack.HotModuleReplacementPlugin()];
+
+module.exports = {
+    entry,
+    plugins,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+            }
+        ]
+    },
+};
